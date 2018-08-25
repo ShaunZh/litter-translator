10466db37d745237206af04e07bf1e4e41761bc7
ShaunZh <308826842@qq.com>
2018-08-26 00:15:10 +0800

    添加详细注释

--- translator.js
+++ translator.js
@@ -1,19 +1,17 @@
 #! /usr/bin/env node
-// 引入需要的模块
+ // 引入需要的模块
 
 const axios = require('axios')
-const Table = require('cli-table2')  //表格输出
+const Table = require('cli-table2') //表格输出
 const API = 'http://fanyi.youdao.com/openapi.do?keyfrom=toaijf&key=868480929&type=data&doctype=json&version=1.1'
 
 var data = {};
 var word;
 
-var table = new Table();
-
 // 没有参数时显示帮助信息
 if (!process.argv[2]) {
   console.log('大人，输入点啥嘛');
-  return ;
+  return;
 } else {
   word = process.argv[2];
   data.params = {
@@ -21,20 +19,36 @@ if (!process.argv[2]) {
   }
 }
 
-axios.get(API, data) 
-     .then (function (response) {
-       let result = {}
-       let result2 = [];
-       result[word] = response.data.translation;
-       if (response.data.basic.explains.length) {
-         result2 = response.data.basic.explains; 
-       } 
-       // 输出表格
-      let table = new Table()
-      table.push(result);
-      console.log(table.toString());
-      console.log(result2);
-     })
-     .catch (function (error) {
-	 console.log('大人, 出错了! 有可能输了小的不知道的词');
-     })
+axios.get(API, data)
+  .then(function (response) {
+    let simpleResult ;
+    let basicResult = [];
+    let webResult = [];
+    let phonetic = {};
+    simpleResult = response.data.translation;
+    if (response.data.basic.explains.length) {
+      basicResult = response.data.basic.explains;
+    }
+    if (response.data.web.length) {
+      webResult = response.data.web;
+    }
+    if (response.data.basic['uk-phonetic'] && response.data.basic['uk-phonetic'].length) {
+      phonetic['uk-phonetic'] = response.data.basic['uk-phonetic'];
+    }
+    if (response.data.basic['us-phonetic'] && response.data.basic['us-phonetic'].length) {
+      phonetic['us-phonetic'] = response.data.basic['us-phonetic'];
+    }
+    // 输出表格
+    let table = new Table()
+    table.push(['美式发音', phonetic['us-phonetic']]);
+    table.push(['英式发音', phonetic['uk-phonetic']]);
+    table.push(['基本释义', simpleResult.join(',')]);
+    table.push(['详细释义', basicResult.join(',')]);
+    for (var i = 0; i < webResult.length && i < 3; i++) {
+      table.push([`'${webResult[i].key}'`,  webResult[i].value.join(', ')]);
+    }
+    console.log(table.toString());
+  })
+  .catch(function (error) {
+    console.log(`error: ${error.message}`)
+  })
